63671e1ee79a7db2d064fb16577721417b6a7db4
Jackclarify <jack.chen@clarifyhealth.com>
2018-02-27 11:17:54 +0800

    remove export html-outlet

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import { JitCompilerFactory } from "@angular/platform-browser-dynamic";
 import { CommonModule } from '@angular/common';
 import { HtmlOutlet } from './html-outlet';
 
-export * from './html-outlet';
-
 export function createCompiler(compilerFactory: CompilerFactory) {
     return compilerFactory.createCompiler();
 }
